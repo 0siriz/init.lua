b4d89344d557456c89b41367fac56b9ab3015fb3
0siriz <0siriz@proton.me>
2024-11-24 19:12:56 +0100

    Plugin update and minor changes

--- lua/osiriz/autocmds.lua
+++ lua/osiriz/autocmds.lua
@@ -1,9 +1,9 @@
 -- Number toggle
-local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
+local numbertogglegroup = vim.api.nvim_create_augroup('numbertoggle', {})
 
 vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'CmdlineLeave', 'WinEnter' }, {
 	pattern = '*',
-	group = augroup,
+	group = numbertogglegroup,
 	callback = function()
 		if vim.o.nu and vim.api.nvim_get_mode().mode ~= 'i' then
 			vim.opt.relativenumber = true
@@ -13,7 +13,7 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'Cmdline
 
 vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'CmdlineEnter', 'WinLeave' }, {
 	pattern = '*',
-	group = augroup,
+	group = numbertogglegroup,
 	callback = function(event)
 		if vim.o.nu then
 			vim.opt.relativenumber = false
