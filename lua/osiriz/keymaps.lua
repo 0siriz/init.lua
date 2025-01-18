2e4122bb3134f537d7a7f1947af942a64e2fc822
0siriz <0siriz@proton.me>
2025-01-18 18:40:10 +0100

    Added more diagnostic keybinds

--- lua/osiriz/keymaps.lua
+++ lua/osiriz/keymaps.lua
@@ -4,3 +4,15 @@ vim.keymap.set(
 	vim.diagnostic.open_float,
 	{ desc = '[D]iagnostics [F]loat' }
 )
+vim.keymap.set(
+	'n',
+	'<leader>dn',
+	vim.diagnostic.goto_next,
+	{ desc = '[D]iagnostics Goto [N]ext' }
+)
+vim.keymap.set(
+	'n',
+	'<leader>dp',
+	vim.diagnostic.goto_prev,
+	{ desc = '[D]iagnostics Goto [P]revious' }
+)
