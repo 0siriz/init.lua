63da793c1da615f74ac29f000abf1926b34e0172
0siriz <0siriz@proton.me>
2025-05-21 18:33:13 +0200

    Dont use lsp for folding

--- lua/osiriz/autocmds.lua
+++ lua/osiriz/autocmds.lua
@@ -23,14 +23,3 @@ vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'CmdlineEn
     end
   end
 })
-
--- LSP
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client and client:supports_method('textDocument/foldingRange') then
-      local win = vim.api.nvim_get_current_win()
-      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
-    end
-  end
-})
