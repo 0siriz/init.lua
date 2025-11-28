e0d32a6fec7756af22ff64cbe9822fd4830d110f
0siriz <0siriz@proton.me>
2025-11-28 23:48:13 +0100

    Removed hovercraft in favor of vim.lsp.buf.hover

--- lua/osiriz/keymap.lua
+++ lua/osiriz/keymap.lua
@@ -1,3 +1,4 @@
 -- LSP
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename' })
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Action' })
+vim.keymap.set('n', 'K', function() vim.lsp.buf.hover({ border = 'rounded' }) end, { desc = 'Hover' })
